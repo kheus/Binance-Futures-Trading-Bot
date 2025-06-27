5056749ef634ee42a8a874aae0eae661f2e238a7
kheus <34310748+kheus@users.noreply.github.com>
2025-06-27 15:46:25 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,7 @@
 apt-get update && apt-get install -y build-essential wget
 
 # Installe TA-Lib (librairie C)
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-tar -xvzf ta-lib-0.4.0-src.tar.gz
-cd ta-lib-0.4.0
-./configure --prefix=/usr
-make
-make install
-cd ..
-rm -rf ta-lib-0.4.0*
+pip install ta-lib==0.4.19
 
 # Installe les dépendances Python
 pip install --upgrade pip
